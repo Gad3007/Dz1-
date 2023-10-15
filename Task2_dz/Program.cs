66e37e35d42e4bcc08b51deb095280cc93056a6a
Ageev Ilya <Aii23@mail.ru>
2023-10-15 21:16:33 +0300

    Задача 2

---
+++ Task2_dz/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число ");
+string? num1_str = Console.ReadLine();
+int num1 = Convert.ToInt32(num1_str);
+Console.WriteLine("Введите второе число ");
+string? num2_str = Console.ReadLine();
+int num2 = Convert.ToInt32(num2_str);
+if(num1 > num2)
+{
+    Console.Write("max = " + num1);
+}
+else
+{
+    Console.Write("max = " + num2);
+}
