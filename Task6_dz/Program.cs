5e8cc90b2f7412e1f6168446f48ea8de262e87e6
Ageev Ilya <Aii23@mail.ru>
2023-10-15 22:16:12 +0300

    Задача 6

---
+++ Task6_dz/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число ");
+string? num_str = Console.ReadLine();
+int num = Convert.ToInt32(num_str);
+if (num != 0)
+{
+    int result = num % 2;
+    if(result > 0)
+    {
+        Console.Write("Нет");
+    }
+    else
+    {
+        Console.Write("Да");
+    }
+}
