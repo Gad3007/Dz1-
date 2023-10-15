3efb60ae46a8095722b590b6aeb29e0afddc2e4f
Ageev Ilya <Aii23@mail.ru>
2023-10-15 22:36:15 +0300

    Задача 8

---
+++ Task8_dz/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+string? N_str = Console.ReadLine();
+int N = Convert.ToInt32(N_str);
+for(int i = 1;i < N+1;i++)
+{
+    int result = i % 2;
+    if(result == 0)
+    {
+    Console.Write(i + " "); 
+    }
+}    
